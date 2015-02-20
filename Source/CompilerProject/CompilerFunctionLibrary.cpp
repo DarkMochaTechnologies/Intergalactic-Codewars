6340a926a19c286e8a82a429dd31897eeb2c848b
Carl Tan <carlstephentan@yahoo.com>
2015-02-20 09:05:49 +0800

    Binaries + Updates

--- Source/CompilerProject/CompilerFunctionLibrary.cpp
+++ Source/CompilerProject/CompilerFunctionLibrary.cpp
@@ -3,7 +3,8 @@
 #include "CompilerProject.h"
 #include "CompilerFunctionLibrary.h"
 #include "Regex.h"
-
+#include <string>
+#include "regex"
 
 
 UCompilerFunctionLibrary::UCompilerFunctionLibrary(const class FObjectInitializer& OI)
@@ -14,9 +15,38 @@ UCompilerFunctionLibrary::UCompilerFunctionLibrary(const class FObjectInitialize
 
 /*Gets what command is being initiated.*/
 ECommandsEnum UCompilerFunctionLibrary::ParseCommand(FString Command){
+	//FString to std::string
+	std::string CommandString(TCHAR_TO_UTF8(*Command));
 
-
-	return ECommandsEnum::CE_MoveDown;
+	std::smatch match;
+	//check if moveup()
+	if (std::regex_search(CommandString, match, std::regex("moveup()")) && match.size() > 1){
+		return ECommandsEnum::CE_MoveUp;
+	}
+	//check if movedown()
+	else if (std::regex_search(CommandString, match, std::regex("movedown()")) && match.size() > 1){
+		return ECommandsEnum::CE_MoveDown;
+	}
+	//check if moveleft()
+	else if (std::regex_search(CommandString, match, std::regex("moveleft()")) && match.size() > 1){
+		return ECommandsEnum::CE_MoveLeft;
+	}
+	//check if moveright()
+	else if (std::regex_search(CommandString, match, std::regex("moveright()")) && match.size() > 1){
+		return ECommandsEnum::CE_MoveRight;
+	}
+	else if (std::regex_search(CommandString, match, std::regex("fire(\d\,\d)")) && match.size() > 1){
+		return ECommandsEnum::CE_Fire;
+	}
+	else{
+		return ECommandsEnum::CE_InvalidCommand;
+	}
+	//FRegexPattern MoveUpPattern = FRegexPattern("//d");
+	//FRegexMatcher matcher = FRegexMatcher(MoveUpPattern, Command);
+	//if (matcher.FindNext())
+	//	return ECommandsEnum::CE_MoveUp;
+	//else
+	//	return ECommandsEnum::CE_InvalidCommand;
 }
 
 /*Gets the firing coordinates specified in the Fire command*/
